bf567d41334628f7b71194d6b7e9ecc9b081254e
Roberts Tūbelis <robertstubelis@gmail.com>
2022-12-01 12:22:16 +0200

    design problems

---
+++ photos/static/photos/scripts/script.js
@@ -0,0 +1,5 @@
+let sort_button = document.getElementById("sort-btn");
+console.log(sort_button);
+sort_button.onclick = function(){
+    console.log(1)
+}
--- photos/views.py
+++ photos/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def main(request):
-    return HttpResponse("My photo")
+    return render(request, "photos/index.html")
