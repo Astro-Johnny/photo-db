7379dbfb4ce1cb8a143dc830b3f507f680071f32
Roberts Tūbelis <robertstubelis@gmail.com>
2022-12-05 13:59:56 +0200

    Images now show up, other small ui changes

--- photos/tools/utility.py
+++ photos/tools/utility.py
@@ -1,7 +1,17 @@
 from collections import namedtuple
+import sqlite3
 
 
 def namedtuplefetchall(cursor, name):
     desc = cursor.description
     nt_result = namedtuple(name, [col[0] for col in desc])
     return [nt_result(*row) for row in cursor.fetchall()]
+
+
+def getTableData(tableName):
+    with sqlite3.connect('./db.sqlite3') as con:
+        cursor = con.cursor()
+        cursor.execute(f"SELECT * FROM {tableName}")
+        result = namedtuplefetchall(cursor, f"{tableName}")
+    con.close()
+    return result
--- photos/views.py
+++ photos/views.py
@@ -1,18 +1,10 @@
+from photos.tools.utility import getTableData
 from django.shortcuts import render
-from photos.tools.utility import namedtuplefetchall
-import sqlite3
 
-
-def getTableData(tableName):
-    with sqlite3.connect('./db.sqlite3') as con:
-        cursor = con.cursor()
-        cursor.execute(f"SELECT * FROM {tableName}")
-        result = namedtuplefetchall(cursor, f"{tableName}")
-    con.close()
-    return result
 
 def main(request):
     cameras = getTableData("photos_camera")
     film = getTableData("photos_film")
     event = getTableData("photos_event")
-    return render(request, "photos/index.html", {"cameras": cameras, "film": film, "event": event})
+    photos = getTableData("photos_photos")
+    return render(request, "photos/index.html", {"cameras": cameras, "film": film, "event": event, "photos": photos})
